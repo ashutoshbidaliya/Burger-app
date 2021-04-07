b21b506067ddf4b0860596a4dfcd96f8fad861ae
Ashutosh <ashutoshbidaliya@gmail.com>
2021-04-07 20:16:12 +0530

    Added propTypes to the project

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Auxiliray from '../../hoc/Auxiliary'
+import classes from './Layout.css';
 
 const layout = (props) => (
 <Auxiliray>
 <div>Toolbar, SideDrawer, Backdrop</div>
-<main>
+<main className={classes.Content}>
     {props.children}
 </main>
 </Auxiliray>    
